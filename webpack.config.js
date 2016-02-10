639b705c1aa379978e5d9f72971cdd00e3749fda
Edgaras Mickus <edgaras.mickus@gmail.com>
2016-02-10 11:26:34 +0200

    npm scripts added

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   entry: './app/main.js',
   output: {
-    filename: './public/bundle.js'
+    filename: './public/bundle.js',
+    publicPath: '/',
   },
   module: {
     loaders: [
